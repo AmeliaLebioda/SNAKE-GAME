246f86e21e50579944554bf7b66fe22de3fa24b9
amelialebioda <amelia.lebioda@gmail.com>
2017-11-25 21:43:56 +0100

    centering txt

--- newpixel.py
+++ newpixel.py
@@ -20,10 +20,16 @@ block_size=10
 FPS =30
 
 font = pygame.font.SysFont(None, 25)
+def text_objects(text, color):
+    textSurface=font.render(text, True, color)
+    return textSurface, textSurface.get_rect()
 
 def message_to_screen(msg,color):
-    screen_text = font.render(msg, True, color)
-    gameDisplay.blit(screen_text, [display_width/2,display_height/2])
+    textSurf, textRect = text_objects(msg, color)
+    textRect.center = (display_width/2,display_height/2)
+    gameDisplay.blit(textSurf, textRect)
+    ##screen_text = font.render(msg, True, color)
+    ##gameDisplay.blit(screen_text, [display_width/2,display_height/2])
 
 def snake(block_size, snakelist):
     for XnY in snakelist:
@@ -54,6 +60,10 @@ def gameLoop():
             pygame.display.update()
 
             for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    gameExit==True
+                    gameOver==False
+                
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_q:
                         gameExit = True
