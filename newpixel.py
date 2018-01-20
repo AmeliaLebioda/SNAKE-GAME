245a4cb35eb3a3e6185798440add5f8276d655bf
amelialebioda <amelia.lebioda@gmail.com>
2018-01-20 19:58:48 +0100

    The new head and logo

--- newpixel.py
+++ newpixel.py
@@ -7,28 +7,26 @@ pygame.init()
 white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
-green = (0,155,0)
 
 display_width = 800
 display_height = 600
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('Slither')
-bb=pygame.image.load('purple.png')
-cc = pygame.image.load('blue.png')
-aa = pygame.image.load('green.png')
-ee = pygame.image.load('violet.png')
-dd = pygame.image.load('red.png')
+pygame.display.set_caption('Snake Game')
+logo = pygame.image.load('logo.png')
+headsquare = pygame.image.load('headhead.png')
+greensquare = pygame.image.load('green.png')
+purplesquare = pygame.image.load('purple.png')
+bluesquare = pygame.image.load('blue.png')
+redsquare = pygame.image.load('red.png')
+violetsquare = pygame.image.load('violet.png')
+pygame.display.set_icon(logo)
 
-
-listofsegment = [aa,bb,cc,dd,ee]
-
-
+segmentlist = [greensquare,purplesquare,bluesquare,redsquare,violetsquare]
 clock = pygame.time.Clock()
 
 FPS =8
-    
-
+                       
 direction =  "right"
 smallfont = pygame.font.SysFont("comicsansms", 20)
 medfont = pygame.font.SysFont("comicsansms", 50)
@@ -53,38 +51,24 @@ def message_to_screen(msg,color, y_displace=0, size = "small"):
     textRect.center = (display_width/2),(display_height/2)+ y_displace
     gameDisplay.blit(textSurf, textRect)
 
-
 def snake(block_size, snakelist):
+
+    gameDisplay.blit(headsquare, (snakelist [-1] [0], snakelist [-1][1]))
+    gameDisplay.blit(greensquare, (snakelist [0] [0], snakelist [0][1]))
+
+    segmentcounter=1
     
-    if direction =="right":
-        head = aa
-
-    if direction =="left":
-        head = aa
+    for body in snakelist[1:-1]:
+       
     
-    if direction =="up":
-        head = aa
-
-    if direction =="down":
-        head = aa
-    gameDisplay.blit(head, (snakelist [-1] [0], snakelist [-1][1]))
-    gameDisplay.blit(head, (snakelist [0] [0], snakelist [0][1]))
-
-
-
-    k=1
-    for XnY in snakelist[1:-1]:
+        party = segmentlist [segmentcounter]
+        gameDisplay.blit(party, (body[0],body[1]))
         
+        segmentcounter+=1
+        if segmentcounter == 5:
+            segmentcounter=0
         
-        party = listofsegment [k]
-        gameDisplay.blit(party, (XnY[0],XnY[1]))
-        k=k+1
-        if k == 5:
-            k=0
-        print (k)
     
-   
-
   
 def pause ():
     paused = True
@@ -138,55 +122,39 @@ def start_menu():
          
 def gameLoop():
     global direction
-    k=3
+    segmentcounter=3
     
     gameExit = False
     gameOver = False
-    
-    
 
-    snakeList =[[display_width/2,display_height/2 ]]
+    snakeList =[[display_width/2,display_height/2]]
     
     snakeLenght = 2
 
-    block_size= 25
-    #if snakeLenght <5:
-     #   FPS =20
+    block_size=25
+    #if snakeLenght < 5:
+     #   FPS = 2
     #else:
-        #FPS =20
+     #   FPS = 20
         
-       
-    
-    
-
     lead_x = display_width/2 
     lead_y = display_height/2 
-
- 
-    
-    
     lead_x_change = block_size
     lead_y_change = 0
     
     
     applelocationx = []
-    xapple = 25
-    while xapple<= display_width - 25:
+    xapple = block_size
+    while xapple<= display_width - block_size:
         applelocationx.append(xapple)
-        xapple=xapple+25
+        xapple=xapple+block_size
 
     applelocationy = []
-    yapple = 25
-    while yapple<= display_height - 25:
+    yapple = block_size
+    while yapple<= display_height - block_size:
         applelocationy.append(yapple)
-        yapple=yapple+25
+        yapple=yapple+block_size
 
-    
-        
-       
-   
-    #randAppleX=round (random.randrange(0,display_width-block_size)/10.0)*10.0
-    #randAppleY=round (random.randrange(0,display_height-block_size)/10.0)*10.0
     randAppleX = random.choice(applelocationx)
     randAppleY = random.choice(applelocationy)
     
@@ -195,7 +163,7 @@ def gameLoop():
     while not gameExit:
         if gameOver == True:
             
-            message_to_screen("The game is over",red, -100, size ="large")
+            message_to_screen("GAME OVER",red, -100, size ="large")
             message_to_screen("Press C to play again or Q to quit",black, 50, size = "small")
             
             pygame.display.update()
@@ -247,18 +215,9 @@ def gameLoop():
         gameDisplay.fill(white)
         
         AppleThickness =25
-        #appleimage=random.choice(listofsegment)
-        #appleimage=ee
-        #if snakeLenght == 2:
-         #   appleimage= bb
-        #else:
-         #   appleimage==random.choice(listofsegment)
-
         
- 
-        #pygame.draw.rect(gameDisplay, red, [randAppleX,randAppleY,AppleThickness,AppleThickness])
         if snakeLenght == 2:
-            appleimage= bb
+            appleimage = purplesquare
         gameDisplay.blit(appleimage, (randAppleX,randAppleY))
         snakeHead=[]
         
@@ -266,8 +225,6 @@ def gameLoop():
         snakeHead.append(lead_x)
         snakeHead.append(lead_y)
         snakeList.append(snakeHead)
-        print (snakeList)
-        
        
         if len(snakeList) > snakeLenght:
             del snakeList[0]
@@ -276,21 +233,10 @@ def gameLoop():
         for eachSegment in snakeList [:-1]:
             if eachSegment == snakeHead:
                 gameOver=True
-             
-        
-        #print (lead_x)
-        #print (lead_y)
-        
-        
+       
         score(snakeLenght - 2)
         snake(block_size,snakeList)
         pygame.display.update()
-        
-        
-
-        
-
-        
 
         if lead_x>randAppleX-AppleThickness and lead_x<randAppleX+ AppleThickness:
             if lead_y>randAppleY - AppleThickness and lead_y<randAppleY+AppleThickness:
@@ -298,28 +244,15 @@ def gameLoop():
                randAppleY=random.choice(applelocationy)
                
                snakeLenght += 1
-               k=2
+               segmentcounter=2
                for appleimage in snakeList[:-1]:
-                   appleimage = listofsegment [k]
+                   appleimage = segmentlist[segmentcounter]
                    
-                   k=k+1
-                   if k == 5:
-                       k=0
-        
-
-
-
-     
-               
-               
-        print (snakeLenght)
-        
-        
-               
-      
+                   segmentcounter+=1
+                   if segmentcounter == 5:
+                       segmentcounter=0
         
         clock.tick(FPS)
-
 
     pygame.quit()
     quit()
